a1388e068a1b99c8646584ea665e954a004a3697
raghunandanbs <raghu.bs@partner.samsung.com>
2020-07-20 14:53:38 +0530

    Fix syntax error

--- cmd/apis.go
+++ cmd/apis.go
@@ -4,10 +4,10 @@ import (
 	"fmt" //apiGroup, APIResourcelist
 
 	"k8s.io/klog"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func findApiResources()  (APIResourceList ,error) {
+func findApiResources()  (v1.APIResourceList ,error) {
 	// https://godoc.org/k8s.io/client-go/discovery#DiscoveryInterface
 	apiGroup, apiResourceList, err := discoveryClient.ServerPreferredResources()
 	if err != nil {
