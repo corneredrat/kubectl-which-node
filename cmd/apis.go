b6f778ef45fcbd6c79a9570f810cad5b4e51e603
raghunandanbs <raghu.bs@partner.samsung.com>
2020-07-20 15:07:33 +0530

    Fix syntax error

--- cmd/apis.go
+++ cmd/apis.go
@@ -7,14 +7,14 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func findApiResources()  (v1.APIResourceList ,error) {
+func findApiResources()  ([]*v1.APIResourceList ,error) {
 	// https://godoc.org/k8s.io/client-go/discovery#DiscoveryInterface
 	apiResourceList, err := discoveryClient.ServerPreferredResources()
 	if err != nil {
 		return apiResourceList, fmt.Errorf("unable to fetch api resource list: %w")
 	}
 
-	klog.Info("apiResouceList: %v", apiResourceList.String())
+	klog.Info("apiResouceList: %v", apiResourceList)
 	return apiResourceList, nil
 }
 
