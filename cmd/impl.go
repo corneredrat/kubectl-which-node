88761839efa5a82587e37d1570279c263ce5b1e7
raghunandanbs <raghu.bs@partner.samsung.com>
2020-07-20 11:44:32 +0530

    Add api.go, impl.go

--- cmd/impl.go
+++ cmd/impl.go
@@ -1,12 +1,12 @@
 package cmd
 
 import (
-	""
+	"k8s.io/klog"
 )
 
 func findNodes(kind string, object string) {
-	
-	klog.Info("Kind:%v Object:%v",kind, object)
+
+	klog.Info("Kind:%v Object:%v", kind, object)
 	klog.V(3).Info("Finding API resources")
 	_ = findApiResources()
 }
