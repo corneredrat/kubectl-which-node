70376177e31fca86d743c7b4abcfd2be29d2236f
raghunandanbs <raghu.bs@partner.samsung.com>
2020-07-20 18:32:37 +0530

    Fix syntax error

--- cmd/impl.go
+++ cmd/impl.go
@@ -13,7 +13,11 @@ func findNodes(kind string, object string) error {
 	// Check available "Kinds"
 	availableResourceNames, err := findApiResourceNames()
 	// Check if kind requests exist
-	if !exists(kind, availableResourceNames) {
+	var iKind interface{}
+	var iAvailableResourceNames []interface{}
+	iKind = kind
+	iAvailableResourceNames = availableResourceNames
+	if !exists(iKind, iAvailableResourceNames) {
 		return fmt.Errorf("kind %v is not available in the server.",kind)
 	}
 	klog.V(3).Infof("found kind %v",kind)
